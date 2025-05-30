3d2ee4cf054a13f0beacbbf86c9cb852f221bdc5
stathiskal11 <167206516+stathiskal11@users.noreply.github.com>
2025-05-30 18:32:51 +0300

    Update and rename ReferralListActivity.java to ReferralList.java

--- app/src/main/java/com/example/smartmed1/ReferralList.java
+++ app/src/main/java/com/example/smartmed1/ReferralList.java
@@ -14,7 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ReferralListActivity extends AppCompatActivity {
+public class ReferralList extends AppCompatActivity {
 
     private RecyclerView recyclerViewReferrals;
     private ReferralAdapter referralAdapter;
